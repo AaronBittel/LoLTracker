178e9c65fde180b23823706b9026b1e851974af3
Aaron Bittel <aaron@bittels.de>
2023-11-25 00:49:15 +0100

    Instead of manually added columns to create the dataframe, I just use the first dictionary .keys() method to get all columns for the dataframe

--- apps/backend/src/main.py
+++ apps/backend/src/main.py
@@ -10,13 +10,13 @@ logging.basicConfig(level=logging.INFO, filename="../logging/logging.txt", filem
 
 
 def main():
-    summoner_name = "noway2u"
+    summoner_name = "TRM BROSES"
     server = "EUW1"
-    queue = constants.Queue.RANKED
-    number_of_games = 3000
-    till_season_patch = constants.Patch(12, 1)
+    queue = constants.Queue.ARAM
+    number_of_games = 1
+    till_season_patch = constants.Patch(13, 20)
 
-    path = r"C:\Users\AaronWork\Projects\LoLTracker\apps\data\noway2uEUW.parquet"
+    path = r"C:\Users\AaronWork\Projects\LoLTracker\apps\data\test_data.parquet"
 
     match_info_generator, puuid = game_data_fetcher.create_game_data_generator(
         summoner_name=summoner_name,
@@ -25,12 +25,6 @@ def main():
         number_of_games=number_of_games,
         till_season_patch=till_season_patch,
     )
-
-    players_to_search = [
-        name
-        for name, player_puuid in constants.PLAYERS.items()
-        if player_puuid != puuid
-    ]
 
     player_data_list = []
     for i, game_data in enumerate(match_info_generator, start=1):
@@ -101,27 +95,7 @@ def main():
         player_data_list.append(player_data)
         print(f"{i} Games")
 
-    df = pd.DataFrame(
-        data=player_data_list,
-        columns=(
-            constants.META_DATA_COLUMNS
-            + constants.INFO_DATA_COLUMNS
-            + [
-                "laneOpponent",
-                "gameDuration_m_s",
-                "totalAllyTeamKills",
-                "totalAllyTeamDeaths",
-            ]
-            + players_to_search
-            + constants.PARTICIPANT_DATA_COLUMNS
-            # + constants.ROLES_PICK_COLUMNS
-            + constants.ALLY_TEAM_PICKS
-            + constants.ENEMY_TEAM_PICKS
-            # + constants.ROLES_BAN_COLUMNS
-            + constants.ALLY_TEAM_BANS
-            + constants.ENEMY_TEAM_BANS
-        ),
-    )
+    df = pd.DataFrame(data=player_data_list, columns=list(player_data_list[0].keys()))
 
     data_clean_up.convert_unix_timestamp_ms_to_datetime(
         df=df, cols=["gameCreation", "gameEndTimestamp"]
