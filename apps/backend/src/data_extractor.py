9624b66c089a10600c4d9799b85fc629e344233f
Aaron Bittel <aaron@bittels.de>
2023-11-26 22:28:18 +0100

    some change

---
+++ apps/backend/src/data_extractor.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from apps.backend.src import game_data_fetcher
+from apps.backend.src import constants
+from apps.backend.src import match_data_processor
+from apps.backend.src import time_line_processor
+from apps.helper import helper
+from riotwatcher import LolWatcher
+
+from typing import Generator
+
+
+def create_dataframe(match_data_generator: Generator) -> pd.DataFrame:
+    all_match_data_list = []
+    for match_data, time_line_data in match_data_generator:
+        match_data_dict = match_data_processor.get_info(match_data)
+        time_line_dict = time_line_processor.get_info(time_line)
+        match_data_dict.update(time_line_dict)
+        all_match_data_list.append(match_data_dict)
+    return pd.DataFrame(all_match_data_list, columns=all_match_data_list[0].keys())
+
+
+if __name__ == "__main__":
+    input_values = {
+        "summoner_name": "TRM%20BROSES",
+        "server": "EUW1",
+        "queue": constants.Queue.RANKED,
+        "number_of_games": 1,
+        "till_season_patch": constants.Patch(13, 1),
+    }
+
+    api_key = helper.get_api_key_from_file()
+    watcher = LolWatcher(api_key=api_key)
+
+    match_data_generator = game_data_fetcher.create_match_data_generator(
+        lolwatcher=watcher, **input_values
+    )
+
+    df = create_dataframe(match_data_generator)
---
+++ apps/backend/src/match_data_processor.py
@@ -0,0 +1,91 @@
+from apps.backend.src import constants
+
+
+def get_data(game_data: dict, columns: list[str]):
+    return {col: game_data[col] for col in columns}
+
+
+def get_lane_opponent(participants_game_data: list[dict], participant_index: int):
+    player_team_position = participants_game_data[participant_index]["teamPosition"]
+    for index, participant in enumerate(participants_game_data):
+        if index == participant_index:
+            continue
+        if player_team_position == participant["teamPosition"]:
+            return {"laneOpponent": participant["championName"]}
+
+
+def get_champions_played(participants_game_data: list[dict]):
+    return {
+        role: participant["championName"]
+        for role, participant in zip(
+            constants.ROLES_PICK_COLUMNS, participants_game_data
+        )
+    }
+
+
+def get_champions_played_ally_team_first(
+    participants_game_data: list[dict], team_id: int
+):
+    if team_id == 100:
+        team_order = constants.ALLY_TEAM_PICKS + constants.ENEMY_TEAM_PICKS
+    else:
+        team_order = constants.ENEMY_TEAM_PICKS + constants.ALLY_TEAM_PICKS
+    return {
+        team_role: participant["championName"]
+        for team_role, participant in zip(team_order, participants_game_data)
+    }
+
+
+def get_champions_banned(game_info_data: dict):
+    champion_bans = []
+    for ban in game_info_data["teams"][0]["bans"] + game_info_data["teams"][1]["bans"]:
+        champion_id = ban["championId"]
+        champion_name = constants.ID_CHAMPION_MAPPING.get(champion_id, "NoBan")
+        champion_bans.append(champion_name)
+
+    return {
+        role: champion_name
+        for role, champion_name in zip(constants.ROLES_BAN_COLUMNS, champion_bans)
+    }
+
+
+def get_champions_banned_ally_team_first(game_info_data: dict, team_id: int):
+    champion_bans = []
+    for ban in game_info_data["teams"][0]["bans"] + game_info_data["teams"][1]["bans"]:
+        champion_id = ban["championId"]
+        champion_name = constants.ID_CHAMPION_MAPPING.get(champion_id, "NoBan")
+        champion_bans.append(champion_name)
+
+    if team_id == 100:
+        team_order = constants.ALLY_TEAM_BANS + constants.ENEMY_TEAM_BANS
+    else:
+        team_order = constants.ENEMY_TEAM_BANS + constants.ALLY_TEAM_BANS
+    return {
+        team_role: champion_name
+        for team_role, champion_name in zip(team_order, champion_bans)
+    }
+
+
+def get_puuid_to_look_out_for(
+    participants_list: list[str], puuids: dict[str, str], player_puuid: str
+):
+    return {
+        name: (puuid in participants_list)
+        for name, puuid in puuids.items()
+        if player_puuid != puuid
+    }
+
+
+def get_ally_team_kills_deaths(participants_game_data: list[dict], team_id: int):
+    if team_id == 100:
+        start, end = 0, 5
+    else:
+        start, end = 5, 10
+    return {
+        "totalAllyTeamKills": sum(
+            participants_game_data[i]["kills"] for i in range(start, end)
+        ),
+        "totalAllyTeamDeaths": sum(
+            participants_game_data[i]["deaths"] for i in range(start, end)
+        ),
+    }
